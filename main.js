d5e824e738f3f248ef69ae125af596411dcbc132
Andre Medina <102329062+medinaandre@users.noreply.github.com>
2023-06-13 17:59:00 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+function tocaSom(setorAudio) {
+  const elemento = document.querySelector(setorAudio);
+
+  if (elemento && elemento.localName === "audio") {
+    elemento.play();
+  } else {
+    alert("Elemento não encontrado");
+  }
+}
+
+const listaDeTeclas = document.querySelectorAll(".tecla");
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+  const tecla = listaDeTeclas[contador];
+  const instrumento = tecla.classList[1];
+  const idAudio = `#som_${instrumento}`;
+
+  tecla.onclick = function () {
+    tocaSom(idAudio);
+  };
+
+  tecla.onkeydown = function (evento) {
+    if (evento.code === "Enter" || evento.code === "Space") {
+      tecla.classList.add("ativa");
+    }
+  };
+  tecla.onkeyup = function () {
+    tecla.classList.remove("ativa");
+  };
+}
